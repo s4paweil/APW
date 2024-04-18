4f3dc628bdad4a01468fb86bf2ed7276e859c226
Patrick Weil <P.Weil94@gmail.com>
2024-04-18 13:08:08 +0200

    Woche01

---
+++ Woche01/Aufgabe00/aufgabe00.cpp
@@ -0,0 +1,14 @@
+//
+// Created by patrick on 18.04.24.
+//
+
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main() {
+    string name;
+    cin >> name;
+    cout << "Hello " << name << "!" << endl;
+}
